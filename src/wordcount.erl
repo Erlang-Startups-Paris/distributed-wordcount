4dde269cfd1e88df170980bfcaa8cd1e8de8b1d8
Bernard Notarianni <bernard.notarianni@gmail.com>
2015-02-15 13:19:06 +0100

    compte parallele d'une portion du fichier

--- src/wordcount.erl
+++ src/wordcount.erl
@@ -1,7 +1,8 @@
 -module (wordcount).
 -export ([lines/1]).
 -export ([lines_p/1]).
--export ([count_file/1]).
+-export ([count_file/3]).
+-export ([split_list/2]).
 
 lines (Lines) ->
     D = dict: new (),
@@ -10,12 +11,16 @@ lines (Lines) ->
                   end, D, Lines).
 
 lines_p (Lines) ->
+    lines_p (Lines, 32).
+
+lines_p (Lines, NbProcess) ->
     Parent = self(),
-    lists: map (fun(L) ->
+    Splitted = split_list (Lines, NbProcess),
+    lists: map (fun(BucketOfLines) ->
                         spawn (fun () ->
-                                       Parent ! string (L, dict: new ())
+                                       Parent ! lines (BucketOfLines)
                                end)
-                end, Lines),
+                end, Splitted),
     collect_result (length (Lines), dict: new ()).
 
 collect_result (0, Dict)->
@@ -43,20 +48,58 @@ count ([FirstWord|Other], Dict) ->
               end,
     count (Other, Updated).
             
-    
 
-count_file (Name) ->
-    L = read_file (Name),
+%%
+
+count_file (Name, From, To) ->
+    L = read_file (Name, From, To),
     lines_p (L).
 
 
-read_file (Name) ->
+read_file (Name, From, To) ->
     {ok, Device} = file: open (Name, read),
-    for_each_line (Device, []).
- 
-for_each_line (Device, Accum) ->
+    skip_first_lines  (Device, From-1),
+    for_each_line (Device, From, To, []).
+
+skip_first_lines (_, 0) -> 
+    ok;
+skip_first_lines (Device, N) ->
+    io: get_line (Device, ""),
+    skip_first_lines (Device, N-1).
+
+for_each_line (Device, From, To, Accum) when From > To->
+    close (Device, Accum);
+for_each_line (Device, From, To, Accum) ->
     case io: get_line (Device, "") of
-        eof  -> file:close (Device), Accum;
-        Line -> for_each_line (Device, [Line|Accum])
+        eof  -> close (Device, Accum);
+        Line -> for_each_line (Device, From+1, To, [Line|Accum])
+    end.
+
+close (Device, Accum) ->
+    file: close (Device), Accum.
+
+
+
+%%
+
+split_list (List, N) ->
+    BucketSize = max (floor (length (List) / N), 1),
+    buckets (List, N-1, BucketSize, []).
+
+buckets ([], _, _, Acc) ->
+    Acc;
+buckets (List, 0, _, Acc) ->
+    [List|Acc];
+buckets (List, N, BucketSize, Acc) ->
+    {Bucket, Remaining} = lists: split (BucketSize, List),
+    buckets (Remaining, N-1, BucketSize, [Bucket|Acc]).
+
+
+floor(X) ->
+    T = erlang: trunc(X),
+    case (X - T) of
+        Neg when Neg < 0 -> T - 1;
+        Pos when Pos > 0 -> T;
+        _ -> T
     end.
 
--- test/wordcount_test.erl
+++ test/wordcount_test.erl
@@ -2,6 +2,7 @@
 -test (exports).
 -export ([count_word_in_one_string/0]).
 -export ([count_word_in_several_string/0]).
+-export ([split_list/0]).
 
 count_word_in_one_string() ->
     Line = "hello world hello",
@@ -16,4 +17,9 @@ count_word_in_several_string () ->
          {"hello", 3},
          {"world", 2}],
     E = lists: sort (dict: to_list (wordcount: lines_p (Lines))).
-    
+
+split_list () ->    
+    [[c,d],[a,b]] = wordcount: split_list ([a,b,c,d], 2),
+    [[c,d,e],[a,b]] = wordcount: split_list ([a,b,c,d,e], 2),
+
+    ok.
